67617bd3ec9a6d64069ae54f540d43e1b7d62cc6
sdaloglu <107580744+sdaloglu@users.noreply.github.com>
2024-03-06 21:30:52 +0000

    The main for loop over time is added, at each step grid is updated and printed on the terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
-# include <iostream>    //for input and output
-
+#include <iostream>    //for input and output
+#include "grid.h"
+#include <chrono>
+#include <thread>
 
 
 int main(int argc, char* argv[]){
     /*
-        The main function for the Conway's Game of Life.
+        The main function for the Conway's Game of Life with periodic boundary conditions.
 
         Args:
             argc: int, the number of command line arguments
@@ -15,6 +17,21 @@ int main(int argc, char* argv[]){
     */
     
 
+    // Generate the initial state of the game -- random binary matrix (square grid) of size Size 10x10
+    // This could also be a user input
+    Grid grid(50,5);
+
+    std::cout << "Works fine!" << std::endl;   
+
+    for (int time = 0; time < 50; ++time){
+        std::cout << "Time Step " << time << std::endl;
+        std::cout << "____________________" << std::endl; 
+        grid.printGrid();
+        grid.updateGrid();
+        std::cout << "____________________" << std::endl;  
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Delay for 500ms
+        system("clear"); // Clear the console
+    }
 
 
     return 0;
