9ec5b3ebfe5915fe3cc34858032ac4570bd8b1fe
sdaloglu <107580744+sdaloglu@users.noreply.github.com>
2024-03-06 11:27:11 +0000

    Created a src file to ensure a good project structure

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+# include <iostream>    //for input and output
+
+
+
+int main(int argc, char* argv[]){
+    /*
+        The main function for the Conway's Game of Life.
+
+        Args:
+            argc: int, the number of command line arguments
+            argv: char*, the command line arguments
+
+        Returns:
+            int, the exit code of the program
+    */
+    
+
+
+
+    return 0;
+}
+
+    
