5463fa1a1a62f7bcdec74b3c31144d9443d86e26
sdaloglu <107580744+sdaloglu@users.noreply.github.com>
2024-03-09 13:29:37 +0000

    Checked if nranks is a perfect square. Implemented reorganizeGrid and communicateBoundary functions to the main file.

--- src/main.cpp
+++ src/main.cpp
@@ -22,72 +22,99 @@ int main(int argc, char* argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
 
-
-
-    // Generate the initial state of the game -- random binary matrix (square grid) of size Size 10x10
-    // This could also be a user input file containing a square grid
-
+    // Input the size of the grid from the command line
     int grid_size = std::stoi(argv[1]);    // Size of the grid
-    Grid grid(grid_size,grid_size,5);
-   
+    
+    // 
+    int process_sqrt = static_cast<int>(sqrt(nranks));  // Cast to int
+    // Check if nrank is a perfect square
+    if (process_sqrt*process_sqrt != nranks){
+        std::cerr << "The number of processes must be a perfect square" << std::endl;
+        return 1;
+    }
 
-    // Split the grid into smaller grids (chunks) for each process
-    int chunk_size = grid_size / nranks;    // assume the size of the grid is divisible by the number of processes
-
-    if (grid_size % nranks != 0){
-        std::cerr << "The size of the grid must be divisible by the number of processes" << std::endl;
+    // Check if the grid size is divisible by the number of processes
+    if (grid_size % process_sqrt != 0){
+        std::cerr << "The size of the grid must be divisible by the square root of the number of processes" << std::endl;
         return 1;
     }
 
 
-    if (rank == 0){
+    // Generate the initial state of the game -- random binary matrix (square grid) of size Size 10x10
+    // This could also be a user input file containing a square grid
+    Grid grid(grid_size,grid_size,5);
+   
 
-        // Scatter the grid to all processes
-        MPI_Scatter(grid.data(), chunk_size*grid_size, MPI_INT, 
-                    local_grid.data(), chunk_size*grid_size, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-    else{
-        //Define a local buffer to receive the scattered grid -- randomly filled
-        Grid local_grid(chunk_size, 5);
+    // Reoranize the grid so that each chunk to be scattered is contiguous in memory
+    grid.reorganizeGrid(nranks);
+    // Put a test here to see if the grid is reorganized correctly
+    
+    // The size of each chunk to be scattered
+    int chunk_size = grid_size/sqrt(nranks);
 
-        MPI_Scatter(NULL, chunk_size*grid_size, MPI_INT, 
-                    local_grid.data(), chunk_size*grid_size, MPI_INT, 0, MPI_COMM_WORLD);
+    //Define a local buffer to receive the scattered grid -- randomly filled
+    Grid local_grid(chunk_size,chunk_size, 5);
 
-        // Run the game for 50 time steps in parallel
-        for (int time=0;time<50;++time){
-            // Communicate the boundary cells with the neighboring processes
-            // Halo exchange of size 1 is required -- also periodic boundary conditions
-            // ...
-            // ...
-            // ...
-
-
-            // Do a local update of the grid here
-            local_grid.updateGrid();
-
-        }
-        // Gather the local grids to the global grid
-            MPI_Gather(local_grid.data(), chunk_size*grid.size, MPI_INT, 
-                        grid.data(), chunk_size*grid.size, MPI_INT, 0, MPI_COMM_WORLD);
-    }
+    // Scatter the grid to all processes
+    MPI_Scatter(grid.getGrid(), chunk_size*chunk_size, MPI_INT, 
+                local_grid.getGrid(), chunk_size*chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
     
 
 
-    // Run the game for 50 time steps only for visualization
-    for (int time = 0; time < 50; ++time){
-        std::cout << "Time Step " << time << std::endl;
-        std::cout << "____________________" << std::endl; 
-        grid.printGrid();
-        grid.updateGrid();
-        std::cout << "____________________" << std::endl;  
-        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Delay for 500ms
-        system("clear"); // Clear the console
-    }
+    // Run the game for 50 time steps in parallel
+    for (int time=0;time<50;++time){
+        // Communicate the boundary cells with the neighboring processes
+        // Halo exchange of size 1 is required -- also periodic boundary conditions
+            
+        local_grid.communicateBoundary(rank, nranks);
 
 
+        // Do a local update of the grid here
+        local_grid.updateGrid();
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        // To plot the local grid
+        if (rank == 20){
+            std::cout << "Time Step " << time << std::endl;
+            std::cout << "____________________" << std::endl; 
+            local_grid.printGrid();
+            std::cout << "____________________" << std::endl;  
+            std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Delay for 500ms
+            system("clear"); // Clear the console
+
+            
+        }
+    }    
+
+    // Gather the local grids to the global grid
+    MPI_Gather(local_grid.getGrid(), chunk_size*chunk_size, MPI_INT, 
+                grid.getGrid(), chunk_size*chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    
     MPI_Finalize();
+
+    // if (rank == 0){
+    // //Run the game for 50 time steps only for visualization
+    // for (int time = 0; time < 50; ++time){
+    //     std::cout << "Time Step " << time << std::endl;
+    //     std::cout << "____________________" << std::endl; 
+    //     grid.printGrid();
+    //     grid.updateGrid();
+    //     std::cout << "____________________" << std::endl;  
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Delay for 500ms
+    //     system("clear"); // Clear the console
+    // }
+    // }
+
+    
+    // if (rank == 0){
+    // std::cout << "____________________" << std::endl; 
+    // std::cout << "Final Grid" << std::endl;   
+    // grid.printGrid();
+    // std::cout << "____________________" << std::endl;  
+    // }
     return 0;
+    
 }
 
     
