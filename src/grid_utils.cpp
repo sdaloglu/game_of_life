33b4fef8f150ac6d076b0ab17156922d4bce35eb
sdaloglu <107580744+sdaloglu@users.noreply.github.com>
2024-03-28 18:02:35 +0000

    Added docstring for the utils functions

--- src/grid_utils.cpp
+++ src/grid_utils.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file grid_utils.cpp
+ * @author Sabahattin Mert Daloglu (sm89@cam.ac.uk)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include "grid_utils.h"
 #include "grid.h"
 #include <fstream>
@@ -7,36 +17,41 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Function to read a binary grid from a .txt file.
+ * 
+ * @param filename input file name
+ * @return Grid in the form of grid class
+ */
 Grid readGridFromFile(const std::string &filename) {
   std::ifstream file(filename);
-  if (!file.is_open()) {
+  if (!file.is_open()) {  // Check if the file is opened successfully
     throw std::runtime_error("Could not open file " + filename);
   }
 
-  std::vector<std::string> lines;
-  std::string line;
-  while (std::getline(file, line)) {
-    std::string processedLine;
+  std::vector<std::string> lines;   // Vector to store the lines of the grid
+  std::string line;               // String to store each line of the grid
+  while (std::getline(file, line)) {   // Read the file line by line
+    std::string processedLine;    
     for (char c : line) {
-      if (c != '0' && c != '1') {
+      if (c != '0' && c != '1') {   // Check if the character is valid
         std::ostringstream message;
         message << "Invalid character '" << c << "' detected in grid. Only '0' or '1' are allowed.";
         throw std::runtime_error(message.str());
       }
-      processedLine += c;
+      processedLine += c;  // If the character is valid, add it to the processed line
     }
-    if (!processedLine.empty()) {
-      lines.push_back(processedLine);
+    if (!processedLine.empty()) {  
+      lines.push_back(processedLine);  // If the line is not empty, add it to the vector
     }
   }
 
-  if (lines.empty()) {
+  if (lines.empty()) {  // Check if input file is empty
     throw std::runtime_error("The input file is empty or doesn't contain any valid grid representation.");
   }
 
-  // Ensure all lines are of the same length
   size_t expectedSize = lines[0].size();
-  for (const auto &l : lines) {
+  for (const auto &l : lines) {  // Check if all lines are of the same length (square grid)
     if (l.size() != expectedSize) {
       throw std::runtime_error("Input grid is not a square. All rows must be of the same length.");
     }
@@ -46,7 +61,7 @@ Grid readGridFromFile(const std::string &filename) {
   int *gridArray = new int[expectedSize * expectedSize];
   for (size_t i = 0; i < expectedSize; ++i) {
     for (size_t j = 0; j < expectedSize; ++j) {
-      gridArray[i * expectedSize + j] = lines[i][j] - '0';
+      gridArray[i * expectedSize + j] = lines[i][j] - '0';  // Allocate the grid array
     }
   }
 
@@ -54,7 +69,7 @@ Grid readGridFromFile(const std::string &filename) {
   Grid grid(expectedSize, expectedSize, 42);
   grid.setGrid(gridArray);
 
-  // Clean up
+  // Clean up the temporary array and return the Grid object
   delete[] gridArray;
 
   return grid;
